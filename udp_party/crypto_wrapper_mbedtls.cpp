111953740da1e9656f8f268fa6116f90fa853da7
Lavi <laviben@post.bgu.ac.il>
2025-04-16 16:02:55 +0300

    Fixed static constexpr in crypto_wrapper_mbedtls.cpp
    Added notes from which slide the info is taken.

--- udp_party/crypto_wrapper_mbedtls.cpp
+++ udp_party/crypto_wrapper_mbedtls.cpp
@@ -24,9 +24,12 @@
 
 
 static constexpr size_t PEM_BUFFER_SIZE_BYTES	= 10000;
-static constexpr size_t HASH_SIZE_BYTES			= ?;
-static constexpr size_t IV_SIZE_BYTES			= ?;
-static constexpr size_t GMAC_SIZE_BYTES			= ?;
+# SHA3 512 bits is 64 Bytes (slide 15)
+static constexpr size_t HASH_SIZE_BYTES			= 64;
+# Recommanded IV is 96 bits - 12 Bytes (Slide 12)
+static constexpr size_t IV_SIZE_BYTES			= 12;
+# Output of GMAC is always 128 bits (slide 15)
+static constexpr size_t GMAC_SIZE_BYTES			= 16;
 
 
 int getRandom(void* contextData, BYTE* output, size_t len)
